9e9d3943c0b470b631c9b4e77d7274a361290e03
Theresa Pidutti <treepidutti@gmail.com>
2021-03-06 15:24:35 -0700

    updates to planner

--- Assets/script.js
+++ Assets/script.js
@@ -39,3 +39,17 @@ $(".description").each(function () {
 });
 
 colorHours();
+
+
+// <!-- using a daily planner to create a schedule -->
+// <!-- open the planner -->
+// <!-- the current day is displayed at the top of the calendar -->
+// <!-- scroll down -->
+// <!-- presented with timeblocks for standard business hours -->
+// <!-- view the timeblocks for that day -->
+// <!-- each timeblock is color coded to indicate whether it is in the past, present, or future -->
+// <!-- click into a timeblock -->
+// <!-- can enter an event -->
+// <!-- click the save button for that timeblock -->
+// <!-- text for that event is saved in local storage -->
+// <!-- refresh the page saved events persist -->
